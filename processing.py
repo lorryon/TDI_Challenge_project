48841c5f7ec61f5daf08ec8216c14b9490582868
lorryon <44561922+lorryon@users.noreply.github.com>
2018-10-29 12:45:11 -0400

    Update processing.py

--- processing.py
+++ processing.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
 
-Data = pd.read_csv('douban_top_250_full.csv')
+Data = pd.read_csv('douban_top_250_full.csv')                        # Read in csv file compiled from crawling Douban and IMDB
 Data.columns = ['Douban Ranking', 
                 'Douban Rating', 
                 'Douban Rating Detail', 
@@ -14,7 +14,7 @@ Data.columns = ['Douban Ranking',
                 'IMDB Rating', 
                 'IMDB Rating Detail', 
                 'IMDB Review Count']
-Data['IMDB Review Count'] = Data['IMDB Review Count'].str.replace(',', '')
+Data['IMDB Review Count'] = Data['IMDB Review Count'].str.replace(',', '')      # Remove comma and change to int
 Data['IMDB Review Count'] = Data['IMDB Review Count'].astype(int)
 x = list(Data['IMDB Rating'])
 y = list(Data['Douban Rating'])
@@ -23,6 +23,6 @@ Y = np.linspace(7, 10)
 plt.scatter(x, y)
 plt.plot(X, Y)
 plt.xlabel("IMDB Rating")
-plt.ylabel('Douban Rating')
+plt.ylabel('Douban Rating')                                         # Scatter plot Douban ratings vs IMDB ratings
 
-
+Data[['Douban Review Count', 'IMDB Review Count']].plot()           # Plot review and compare count from two websites
