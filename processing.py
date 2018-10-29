255f3b5473a19f42f1c4289118e745913d933700
lorryon <44561922+lorryon@users.noreply.github.com>
2018-10-29 12:39:40 -0400

    Update processing.py

--- processing.py
+++ processing.py
@@ -4,4 +4,25 @@ import matplotlib
 from matplotlib import pyplot as plt
 
 Data = pd.read_csv('douban_top_250_full.csv')
-Data[['Douban Rating', 'IMDB Rating']].plot()
+Data.columns = ['Douban Ranking', 
+                'Douban Rating', 
+                'Douban Rating Detail', 
+                'Douban Review Count', 
+                'IMDB URL', 
+                'Movie Name', 
+                'Movie Genre', 
+                'IMDB Rating', 
+                'IMDB Rating Detail', 
+                'IMDB Review Count']
+Data['IMDB Review Count'] = Data['IMDB Review Count'].str.replace(',', '')
+Data['IMDB Review Count'] = Data['IMDB Review Count'].astype(int)
+x = list(Data['IMDB Rating'])
+y = list(Data['Douban Rating'])
+X = np.linspace(7, 10)
+Y = np.linspace(7, 10)
+plt.scatter(x, y)
+plt.plot(X, Y)
+plt.xlabel("IMDB Rating")
+plt.ylabel('Douban Rating')
+
+
